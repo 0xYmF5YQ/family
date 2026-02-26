c5a187d3cf1a9aea5e603b3c82b0901227e3a713
chembeu <simonchembeu@gmail.com>
2026-02-26 08:31:38 +0300

    model registered to admin

--- lineage/admin.py
+++ lineage/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import EventType, Event, Contribution, Asset, Person
+from .models import EventType, Event, Contribution, Asset, Person, AssetOwnership
 
 admin.site.register(Person)
+admin.site.register(EventType)
+admin.site.register(Event)
+admin.site.register(Contribution)
+admin.site.register(Asset)
+admin.site.register(AssetOwnership)
